23f5ec364a17052beb66aa06ceda196bea06ea85
Matteo Lepur <lepurmatteo@gmail.com>
2024-07-29 18:01:34 -0700

    change script arg

--- scripts/plot_cnv.py
+++ scripts/plot_cnv.py
@@ -18,22 +18,23 @@ def preprocess_for_plotting(df_cor_rc: pd.DataFrame) -> pd.DataFrame:
 
 
 def main(args):
-    plots_dir = Path(args.plots_dir)
-    plots_dir.mkdir(parents=True, exist_ok=True)
+    cnv_plot = Path(args.cnv_plot)
+    cnv_plot.parent.mkdir(parents=True, exist_ok=True)
     
     df_cor_rc = pd.read_csv(args.plasma_corr_read_counts, sep='\t')
     df_bins = preprocess_for_plotting(df_cor_rc)
-    
-    cnv_plot_path = plots_dir.joinpath("plasma_cnv_plot.png")
-    plot_bins(df_bins, cnv_plot_path, y_vars=['copy'])
+    plot_bins(df_bins, cnv_plot, y_vars=['copy'])
 
 
 if __name__ == "__main__":
     from argparse import ArgumentParser
     
+    default0 = "numbers-runs/tfri-pair-4-clone-a/cnv/plasma_corr_read_counts.tsv"
+    default1 = "numbers-runs/tfri-pair-4-clone-a/cnv/plasma_cnv_plot.png"
+    
     parser = ArgumentParser()
-    parser.add_argument("--plasma-corr-read-counts", type=str, required=True)
-    parser.add_argument("--plots-dir", type=str, required=True)
+    parser.add_argument("--plasma-corr-read-counts", type=str, default=default0)
+    parser.add_argument("--cnv-plot", type=str, default=default1)
     cli_args = parser.parse_args()
     
     main(cli_args) 
